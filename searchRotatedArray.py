9f39941399a39e1d58ea0b10db5217c29ae66805
Nicole Douglas <45563263+ixn872@users.noreply.github.com>
2021-03-23 12:17:10 +0200

    Create searchRotatedArray.py

---
+++ searchRotatedArray.py
@@ -0,0 +1,30 @@
+import math
+def binary_search_rotated(arr, key):
+  #TODO: Write - Your - Code
+
+  #Recursive binary search
+  #Start with an array covering the original array
+  #Compare key with the middle of the array
+  #If key> middle divide right, and vice versa
+  def recursive_binary(arr,low,high,key):  
+
+
+    if low>high:
+      return -1
+   
+    middle = int(high/2)
+
+    if arr[low]==key:
+        return low
+    if arr[high]==key:
+       return high
+    if key==arr[middle]:
+      return middle
+    if key>arr[middle]:
+      low=middle
+      
+      return recursive_binary(arr,low,high,key)
+    return recursive_binary(arr,low,middle,key)
+
+  return recursive_binary(arr,0,len(arr)-1,key)
+  # return -1
